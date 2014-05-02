9592cc9c42c01b1a629a56e9b7e4b461a7f5d275
abhinavvaid <abhinav.vaid@yahoo.com>
2014-05-02 15:42:17 +0100

    Adding first running test

---
+++ WebDriverGradleProject/src/main/java/Tests/Google.java
@@ -0,0 +1,28 @@
+package Tests;
+
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Google {
+    WebDriver driver = new FirefoxDriver();
+
+    @Before
+    public void startBrowser(){
+        driver.get("https://google.com");
+    }
+
+    @Test
+    public void searchOnGoogle(){
+        driver.findElement(By.id("gbqfq")).sendKeys("Hi");
+    }
+
+    @After
+    public void closeBrowser(){
+        driver.close();
+    }
+}
