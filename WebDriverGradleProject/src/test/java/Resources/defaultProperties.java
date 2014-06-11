c825702ccf9b2a66f5fd03123469f11e9f7def19
MCS <mcs@o2.com>
2014-06-11 10:25:12 +0100

    Adding runnning cucumber and Page object based tests

--- WebDriverGradleProject/src/test/java/Resources/defaultProperties.java
+++ WebDriverGradleProject/src/test/java/Resources/defaultProperties.java
@@ -6,6 +6,6 @@ package Resources;
 public interface defaultProperties {
 
     String driverName = "chrome";
-    String chromeDriverDir = "/WebDriverGradleProject/src/Test/java/Resources/chromedriver";
+    String chromeDriverDir = "/WebDriverGradleProject/src/test/java/Resources/chromedriver";
     String URL = "http://the-internet.herokuapp.com/";
 }
