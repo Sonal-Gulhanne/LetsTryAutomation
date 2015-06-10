4da757bb3dbe96aba59115b14708b7da20a7592a
Abhinav Vaid <abhinav.vaid@yahoo.com>
2015-06-10 10:46:09 +0100

    [AV] - Adding dockingHeader test

--- WebDriverGradleProject/src/test/java/Tests/EcommTests.java
+++ WebDriverGradleProject/src/test/java/Tests/EcommTests.java
@@ -2,15 +2,24 @@ package Tests;
 
 import org.junit.Before;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import java.awt.*;
+import java.awt.event.KeyEvent;
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
@@ -28,7 +37,7 @@ public class EcommTests {
     public void setUp() throws Exception {
         System.setProperty("webdriver.chrome.driver", "/home/ee/projects/o2/LetsTryAutomation/WebDriverGradleProject/src/test/java/Resources/chromedriver");
         driver = new FirefoxDriver();
-        baseUrl = "https://www.ref.o2.co.uk/";
+        baseUrl = "https://localhost.o2.co.uk:9443/upgrade/store";
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     }
 
@@ -38,7 +47,7 @@ public class EcommTests {
         Random random = new Random();
         int randomInt = random.nextInt();
 
-        driver.get(baseUrl + "/shop/phones#page=1");
+        driver.get(baseUrl + "/phones#page=1");
         driver.findElement(By.cssSelector("img[alt=\"Image of the Samsung Galaxy S5\"]")).click();
         driver.findElement(By.id("postPayCTA")).click();
         driver.findElement(By.cssSelector("input.btn-style.btn-primary-dark")).click();
@@ -103,8 +112,27 @@ public class EcommTests {
         driver.findElement(By.id("btnPayNow")).click();
     }
 
+    @Test
+    public void checkDockingHeader() throws AWTException {
+        driver.get("https://www.ref.o2.co.uk/shop/phones/apple/iphone-6/#contractType=paymonthly");
+        Assert.assertTrue(new WebDriverWait(driver, 1).until(ExpectedConditions.invisibilityOfElementLocated(By.xpath("//div[@class='header-container docked']"))));
+        JavascriptExecutor jse = (JavascriptExecutor)driver;
+        jse.executeScript("window.scrollBy(0,1000)", "");
+
+//        Robot robot = new Robot();
+//        robot.keyPress(KeyEvent.VK_PAGE_DOWN);
+//        robot.keyRelease(KeyEvent.VK_PAGE_DOWN);
+
+//        driver.findElement(By.id("reviews")).sendKeys(Keys.PAGE_DOWN);
+
+        Actions actions = new Actions(driver);
+        actions.sendKeys(Keys.BACK_SPACE).perform();
+
+        Assert.assertTrue(new WebDriverWait(driver, 10).until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='header-container docked']"))).isDisplayed());
+    }
+
     @AfterTest()
     public void closeBrowser() {
-//        driver.close();
+        driver.close();
     }
 }
